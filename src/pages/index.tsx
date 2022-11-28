7680470af855833bed0f2b7d254d4b2df16726ce
mrpitch <magnus.volkmann@accenture.com>
2022-11-28 18:29:48 +0100

    feat: add container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+    <Head>
+      <title>AEM Headless PWA</title>
+      <meta
+					name="description"
+					content="Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua."
+				/>
+    </Head>
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-4xl">
+      <h1 className="text-primary-700 text-2xl font-semibold">AEM Headless PWA</h1>
+      </div>
+    </main>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+		extend: {
+			colors: {
+				primary: colors.indigo,
+				secondary: colors.rose,
+				gray: colors.zinc,
+			},
+		},
+	},
   plugins: [],
 }
